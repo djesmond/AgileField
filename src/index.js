92d8ddcb57f68800aa03c27b726b6bb955ecbaa5
Niclas Sommer <Sommer@Sommers-MacBook-Pro.local>
2017-01-22 18:39:06 +0100

    Expose NumberField as export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import AgileTextField from './AgileTextField';
 import PasswordField from './PasswordField';
+import NumberField from './NumberField';
 export {AgileTextField};
 export {PasswordField};
+export {NumberField};

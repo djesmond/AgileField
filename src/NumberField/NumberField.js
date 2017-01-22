728b47efa1cbd7286820327bf2fecf62e16aad4f
Niclas Sommer <Sommer@Sommers-MacBook-Pro.local>
2017-01-22 18:38:32 +0100

    Added NumberField

---
+++ src/NumberField/NumberField.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import AgileTextField from '../AgileTextField';
+import numberValidator from '../Utils/Validators/number';
+
+const styles = {
+};
+
+class NumberField extends Component {
+  render() {
+    const {...passThroughProps} = this.props;
+    return (
+      <AgileTextField
+        type="text"
+        validateInput={true}
+        {...passThroughProps}
+      />
+    );
+  }
+};
+NumberField.propTypes = {
+  name: React.PropTypes.string,
+  label: React.PropTypes.string.isRequired,
+  hintText: React.PropTypes.string,
+  onStateChange: React.PropTypes.func,
+  onValueChange: React.PropTypes.func,
+  feedbackElement: React.PropTypes.func,
+  style: React.PropTypes.object,
+  disabled: React.PropTypes.bool,
+};
+// Specifies the default values for props:
+NumberField.defaultProps = {
+  name: 'number',
+  validator: numberValidator,
+  onStateChange: (state) => {return state},
+  onValueChange: (state) => {return state},
+  style: styles,
+  disabled: false,
+};
+export default NumberField;

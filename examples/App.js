43f3d7b5df0cd4dcf4c8a72e6619442c2ec0345f
Niclas Sommer <Sommer@Sommers-MacBook-Pro.local>
2017-01-22 18:38:48 +0100

    Added NumberField to examples

--- examples/App.js
+++ examples/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import {AgileTextField, PasswordField} from '../src/index';
+import {AgileTextField, PasswordField, NumberField} from '../src/index';
 
 function customValidator(input) {
   if(input.length > 6) {
@@ -57,51 +57,51 @@ class App extends Component {
       <div className="App">
         <h1>Agile Fields</h1>
         <div className="text">
-        <h2>Text</h2>
-        <p>A simple input with a label and hintText</p>
-        <AgileTextField
-          label="Name"
-          hintText="Enter your full name"
-        />
-      <p>Without hint text</p>
-        <AgileTextField
-          label="Name"
-        />
-      <p>Marked as optional</p>
-        <AgileTextField
-          label="Location"
-          optional={true}
-        />
-      <p>One with validation</p>
-        <AgileTextField
-          type="text"
-          label="Name"
-          hintText="Enter your full name"
-          validateInput={true}
-        />
-      <p>One with custom validation</p>
-        <AgileTextField
-          label="Text"
-          hintText="Explain your life with less than 6 characters"
-          validateInput={true}
-          validator={customValidator}
-        />
-      <p>One with validation and value return when value changes</p>
-      <p>Value of input is: {this.state.value}</p>
-        <AgileTextField
-          label="Name"
-          hintText="Enter your full name"
-          validateInput={true}
-          onValueChange={this.handleValueChange}
+          <h2>Text</h2>
+          <p>A simple input with a label and hintText</p>
+          <AgileTextField
+            label="Name"
+            hintText="Enter your full name"
           />
-      <p>One with custom style</p>
-        <AgileTextField
-          type="text"
-          label="Developer doing design"
-          hintText="Please don't tell the designers"
-          style={customStyle}
-        />
-      <p>One with custom feedback element</p>
+          <p>Without hint text</p>
+          <AgileTextField
+            label="Name"
+          />
+          <p>Marked as optional</p>
+          <AgileTextField
+            label="Location"
+            optional={true}
+          />
+          <p>One with validation</p>
+          <AgileTextField
+            type="text"
+            label="Name"
+            hintText="Enter your full name"
+            validateInput={true}
+          />
+          <p>One with custom validation</p>
+          <AgileTextField
+            label="Text"
+            hintText="Explain your life with less than 6 characters"
+            validateInput={true}
+            validator={customValidator}
+          />
+          <p>One with validation and value return when value changes</p>
+          <p>Value of input is: {this.state.value}</p>
+          <AgileTextField
+            label="Name"
+            hintText="Enter your full name"
+            validateInput={true}
+            onValueChange={this.handleValueChange}
+          />
+          <p>One with custom style</p>
+          <AgileTextField
+            type="text"
+            label="Developer doing design"
+            hintText="Please don't tell the designers"
+            style={customStyle}
+          />
+          <p>One with custom feedback element</p>
           <AgileTextField
             type="text"
             label="Just a label"
@@ -109,11 +109,11 @@ class App extends Component {
             validateInput={true}
             feedbackElement={CustomFeedbackElement}
           />
-        <p>Disabled</p>
-        <AgileTextField
-          label="Add to account"
-          hintText="How much money do you want?"
-          disabled={true}
+          <p>Disabled</p>
+          <AgileTextField
+            label="Add to account"
+            hintText="How much money do you want?"
+            disabled={true}
           />
         </div>
         <div className="password">
@@ -121,6 +121,12 @@ class App extends Component {
           <PasswordField
             label="Password"
             hintText="Create a password"
+          />
+        </div>
+        <div className="number">
+          <h2>Number</h2>
+          <NumberField
+            label="Number"
           />
         </div>
       </div>
